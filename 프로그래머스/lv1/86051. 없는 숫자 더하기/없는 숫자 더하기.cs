c0d48fb57b1a54d5d5a08ed15cfdd2c5b3d14fd2
hwon <132995834+phw97123@users.noreply.github.com>
2023-08-29 09:58:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 1.15 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cs
@@ -1,16 +1,8 @@
 using System;
-using System.Linq;
 
 public class Solution {
-    public int solution(int[] numbers)
-            {
-                int answer = 0;
-
-                for (int i = 0; i < 10; i++)
-                {
-                    answer += i; 
-                }
-                answer -= numbers.Sum(); 
-                return answer;
-            }
+    public int solution(int[] numbers) {
+        int answer = -1;
+        return answer;
+    }
 }
