0223a0cd3a67a9d9e8b2420b4c66b8216eab2fa6
hwon <132995834+phw97123@users.noreply.github.com>
2023-10-05 13:08:09 +0900

    [unrated] Title: 공원 산책, Time: 1.11 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.cs
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.cs
@@ -1,62 +1,56 @@
 using System;
-using System.Collections.Generic; 
-    public class Solution
-    {
-        public struct Point
-        {
-            public int x, y;
-        }
+using System.Collections.Generic;
 
-        Point[] move = new Point[]
-        {
-            new Point {x = -1, y = 0},
-            new Point {x = 1, y = 0},
-            new Point {x = 0, y =-1},
-            new Point {x = 0, y = 1},
-        };
+ public class Solution
+ {
+     Dictionary<char, int> mapping = new Dictionary<char, int>() { { 'N', 0 }, { 'S', 1 }, { 'W', 2 }, { 'E', 3 } };
 
-        Dictionary<char, int> mapping = new Dictionary<char, int>() { { 'N', 0 }, { 'S', 1 }, { 'W', 2 }, { 'E', 3 } };
+     public int[] solution(string[] park, string[] routes)
+     {
+             int[] dx = { -1, 1, 0, 0 };
+   int[] dy = { 0, 0, -1, 1 };
 
-        public int[] solution(string[] park, string[] routes)
-        {
-            int H = park.Length;
-            int W = park[0].Length;
+         int[] location = new int[2]; 
 
-            Point location = new Point();
+         int H = park.Length;
+         int W = park[0].Length;
 
-            for (int i = 0; i < H; i++)
-            {
-                for (int j = 0; j < W; j++)
-                {
-                    if (park[i][j] == 'S')
-                    {
-                        location = new Point { x = i, y = j };
-                        break;
-                    }
-                }
-            }
+         //시작위치 
+         for (int i = 0; i < H; i++)
+         {
+             for (int j = 0; j < W; j++)
+             {
+                 if (park[i][j] == 'S')
+                 {
+                     location[0] = i;
+                     location[1] = j; 
+                     break;
+                 }
+             }
+         }
 
-            foreach (var route in routes)
-            {
-                char op = route[0];
-                int n = route[2] - '0';
+         foreach (var route in routes)
+         {
+             char op = route[0];
+             int n = route[2] - '0';
 
-                int nx = location.x;
-                int ny = location.y;
+             int nx = location[0];
+             int ny = location[1];
 
-                while (n-- > 0)
-                {
-                    nx += move[mapping[op]].x;
-                    ny += move[mapping[op]].y;
+             while (n-- > 0)
+             {
+                 nx += dx[mapping[op]];
+                 ny += dy[mapping[op]]; 
 
-                    if ((nx < 0 || H <= nx || ny < 0 || W <= ny) || park[nx][ny] == 'X')
-                        break;
-                }
-                if (n < 0)
-                    location = new Point { x = nx, y = ny };
-            }
-            return new int[] { location.x, location.y };
-        }
-
-      
-    }
+                 if ((nx < 0 || H <= nx || ny < 0 || W <= ny) || park[nx][ny] == 'X')
+                     break;
+             }
+             if (n < 0)
+             {
+                 location[0] = nx;
+                 location[1] = ny; 
+             }
+         }
+         return location; 
+     }
+ }
