581c8ac7891bc9e2959dab3a4ef7eb94a32cb06d
hwon <132995834+phw97123@users.noreply.github.com>
2023-10-17 09:13:14 +0900

    [level 2] Title: 예상 대진표, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Solution
+{
+       public int solution(int n, int a, int b)
+  {
+      int answer =0;
+
+      while( a !=b )
+      {
+          a = (a + 1) >> 1;
+          b = (b + 1) >> 1; 
+          answer++;
+      }
+      return answer;
+  }
+}
