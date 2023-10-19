67c2bce06b11c188e6d6849fcce93b037e8590b7
hwon <132995834+phw97123@users.noreply.github.com>
2023-10-19 10:48:23 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cs
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cs
@@ -1,16 +1,6 @@
 public class Solution {
-     public long solution(int n)
- {
-     int[] result = new int[n+1];
-
-     result[0] = 1; 
-     result[1] = 1;
-     
-     for (int i = 2;  i <= n; i++)
-     {
-         result[i] = (result[i - 1] + result[i - 2]) % 1234567;
-     }
-
-     return result[n];
- }
+    public long solution(int n) {
+        long answer = 0;
+        return answer;
+    }
 }
