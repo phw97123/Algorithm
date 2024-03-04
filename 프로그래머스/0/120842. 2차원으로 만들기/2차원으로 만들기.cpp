3623b2099ee2c6212b067b14147f83eec8463a2e
hwon <132995834+phw97123@users.noreply.github.com>
2024-03-04 16:06:46 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<int> num_list, int n) {
+    int r = num_list.size()/n; 
+    vector<vector<int>> answer(r,vector<int>(n));
+    int index = 0; 
+    for(int i = 0; i<r; i++)
+    {
+        for(int j = 0; j<n; j++)
+        {
+            answer[i][j] = num_list[index++]; 
+        }
+    }
+    return answer;
+}
