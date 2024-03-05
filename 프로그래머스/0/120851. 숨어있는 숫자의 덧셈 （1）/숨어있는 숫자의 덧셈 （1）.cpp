7669ccd80ac4fcc6d5f2ad7082271c1204fbbc85
hwon <132995834+phw97123@users.noreply.github.com>
2024-03-05 10:31:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+
+    for (auto c : my_string)
+    {
+        if (isdigit(c))
+        {
+            answer += c - '0'; 
+        }
+    }
+    return answer;
+}
