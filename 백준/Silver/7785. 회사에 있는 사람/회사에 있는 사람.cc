9e20c832d227bb893e71c903a21cbb500dd0ec7f
hwon <132995834+phw97123@users.noreply.github.com>
2024-08-14 22:28:29 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 112 ms, Memory: 12768 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,29 +1,30 @@
-#include <bits/stdc++.h>
+#include<iostream>
 #include<unordered_set>
+#include<vector>
+#include<algorithm>
+#include<string>
 
 using namespace std;
 
-unordered_set<string> logs;
-
 int main()
 {
-	int num; 
-	cin >> num; 
-	
-	while (num--)
-	{
-		string name, state; 
-		cin >> name >> state; 
-
-		if (state == "enter")
-			logs.insert(name);
-		else
-			logs.erase(name); 
-	}
-
-	vector<string> names(logs.begin(), logs.end()); 
-	sort(names.begin(), names.end(), greater<string>()); 
-	for (auto name : names)
-		cout << name<<'\n';
+    unordered_set<string> logs;
+    int num; 
+    cin>>num; 
+    
+    while(num--)
+    {
+        string name,state; 
+        cin>>name>>state;
+        
+        if(state == "enter")
+            logs.insert(name);
+        else 
+            logs.erase(name);
+    }
+    
+    vector<string> names(logs.begin(), logs.end());
+    sort(names.begin(),names.end(),greater<string>()); 
+    for(auto name : names) cout<<name<<'\n';
+    
 }
-
