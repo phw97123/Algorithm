9176af6901178d67027719599c804163c998f2a5
hwon <132995834+phw97123@users.noreply.github.com>
2024-09-04 19:44:35 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int cnt = 0; 
+	while (n >= 0)
+	{
+		if (n % 5 == 0)
+		{
+			cnt+= (n/5); 
+			cout << cnt; 
+			return 0; 
+		}
+		n -= 3; 
+		cnt++; 
+	}
+	cout << -1; 
+}
